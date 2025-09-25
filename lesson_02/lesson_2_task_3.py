24cb350c9478443b4ff0c18c8e8699085a8ab2fd
kazantip404 <kazantip405@mail.ru>
2025-09-25 22:34:56 +0300

    lesson_02

--- lesson_02/lesson_2_task_3.py
+++
@@ -1,5 +0,0 @@
-def square(side):
-    return side * side
-side_length = 5.5
-area = square(side_length)
-print(f"Площадь квадрата со стороной {side_length} равна {area}")
--- lesson_02/lesson_2_task_6.py
+++
@@ -1,5 +0,0 @@
-lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
-print("\n <30 и делится на 3")
-result = [number for number in lst if number < 30 and number % 3 == 0]
-print(result)
-
