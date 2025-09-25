1d99f653def2a325268b05f344b170451c399217
kazantip404 <kazantip405@mail.ru>
2025-09-25 21:31:14 +0300

    lesson_02

---
+++ lesson_02/lesson_2_task_3.py
@@ -0,0 +1,5 @@
+def square(side):
+    return side * side
+side_length = 5.5
+area = square(side_length)
+print(f"Площадь квадрата со стороной {side_length} равна {area}")
---
+++ lesson_02/lesson_2_task_7.py
@@ -0,0 +1,3 @@
+my_list = list(range(18, 0, -4))
+print("Список:", my_list)
+print("Проверка:", my_list == [18, 14, 10, 6, 2])
