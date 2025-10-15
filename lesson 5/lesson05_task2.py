f9b242df40bedab66cd1716a95702ccf18c1d3d1
kazantip404 <kazantip405@mail.ru>
2025-10-15 22:52:15 +0300

    s

---
+++ lesson 5/lesson05_task2.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+try:
+    driver.get("http://uitestingplayground.com/dynamicid")
+    blue_button = driver.find_element(By.XPATH, "//button[text()='Button with Dynamic ID']")
+    blue_button.click()
+    time.sleep(3)
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
